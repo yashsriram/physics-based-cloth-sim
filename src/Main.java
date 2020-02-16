238a2fe8aa786621829d7854f38cc432d9e51265
Yashasvi Sriram <yash.3997@gmail.com>
2020-02-15 20:37:48 -0600

    improves UserControlledBall class, adds gravity toggling to the ball, positions the ball on the cloth on startup

--- src/Main.java
+++ src/Main.java
@@ -31,13 +31,14 @@ public class Main extends PApplet {
                 1.5f, -50f, -50f, -100f,
                 (i, j, m, n) -> ((i == 0 || i == m - 1) && (j == n - 1 || j % 5 == 0)),
                 GridSpringMassSystem.Layout.ZX);
-        userControlledBall = new UserControlledBall(this, 10, Vec3.of(0, 20, -80), Vec3.of(0, 128, 0));
+        userControlledBall = new UserControlledBall(this, 10, Vec3.of(-10, -100, -60), Vec3.of(0, 128, 0));
     }
 
     public void draw() {
 //        liamCam.Update(1.0f / frameRate);
+
         long start = millis();
-        // cloth update
+        // update
         try {
             for (int i = 0; i < 140; ++i) {
                 gridSpringMassSystem.update(userControlledBall, 0.002f);
@@ -46,10 +47,11 @@ public class Main extends PApplet {
             e.printStackTrace();
         }
         long update = millis();
-
+        // draw
         background(0);
         gridSpringMassSystem.draw();
         long draw = millis();
+
         surface.setTitle("Processing - FPS: " + Math.round(frameRate) + " Update: " + (update - start) + "ms Draw " + (draw - update) + "ms");
     }
 
--- src/physical/UserControlledBall.java
+++ src/physical/UserControlledBall.java
@@ -4,25 +4,29 @@ import linalg.Vec3;
 import processing.core.PApplet;
 
 public class UserControlledBall {
+    final static Vec3 gravity = Vec3.of(0, 5, 0);
     final PApplet parent;
     float radius;
+    final Vec3 initialPosition;
     Vec3 position;
+    Vec3 velocity;
+    Vec3 acceleration;
     Vec3 color;
+    boolean isUnderFreeFall;
 
     public UserControlledBall(PApplet parent, float radius, Vec3 position, Vec3 color) {
         this.parent = parent;
         this.radius = radius;
+        this.initialPosition = position;
         this.position = position;
+        this.velocity = Vec3.zero();
+        this.acceleration = gravity;
         this.color = color;
+        this.isUnderFreeFall = false;
         parent.registerMethod("draw", this);
     }
 
-    public void update(Vec3 velocity, float dt) {
-        position = position.plus(velocity.scale(dt));
-    }
-
     public void draw() {
-        // ball update
         if (parent.keyPressed) {
             switch (parent.key) {
                 case '8':
@@ -43,7 +47,21 @@ public class UserControlledBall {
                 case '9':
                     update(Vec3.of(0, -10, 0), 0.05f);
                     break;
+                case 'r':
+                    position = initialPosition;
+                    velocity = Vec3.zero();
+                    isUnderFreeFall = false;
+                    break;
+                case 'g':
+                    isUnderFreeFall = true;
+                    break;
+                case 'h':
+                    isUnderFreeFall = false;
+                    break;
             }
+        }
+        if (isUnderFreeFall) {
+            update(0.05f);
         }
 
         parent.pushMatrix();
@@ -54,4 +72,12 @@ public class UserControlledBall {
         parent.popMatrix();
     }
 
+    private void update(Vec3 instantaneousVelocity, float dt) {
+        position = position.plus(instantaneousVelocity.scale(dt));
+    }
+
+    private void update(float dt) {
+        position = position.plus(velocity.scale(dt));
+        velocity = velocity.plus(acceleration.scale(dt));
+    }
 }
