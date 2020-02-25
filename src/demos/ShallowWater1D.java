ba2b31e6c5436e0224767d00b360510be23cb911
Yashasvi Sriram <yash.3997@gmail.com>
2020-02-25 03:16:07 -0600

    cleans code

--- src/demos/ShallowWater1D.java
+++ src/demos/ShallowWater1D.java
@@ -7,7 +7,7 @@ import processing.core.PApplet;
 
 import java.util.ArrayList;
 
-public class WaterSimulation extends PApplet {
+public class ShallowWater1D extends PApplet {
     public static final int WIDTH = 800;
     public static final int HEIGHT = 800;
 
@@ -17,7 +17,7 @@ public class WaterSimulation extends PApplet {
     private int numColumns = 60;
     float lengthX = 80, lengthY = 60, lengthZ = 200;
     private float dz;
-    private boolean updateClick = false;
+    private boolean isPlaying = false;
 
     public void settings() {
         size(WIDTH, HEIGHT, P3D);
@@ -106,6 +106,9 @@ public class WaterSimulation extends PApplet {
     void updateLoop() {
         float dt = 0.01f;
         float g = 1f;
+        float damp = 0.1f;
+
+        // Half step
         for (int i = 0; i < numColumns - 1; i++) {
             WaterColumn w1 = waterColumns.get(i);
             WaterColumn w2 = waterColumns.get(i + 1);
@@ -116,49 +119,50 @@ public class WaterSimulation extends PApplet {
             w1.midpointMomentum = (w1.momentum + w2.momentum) / 2f;
             w1.midpointMomentum += -(dt / 2f) * (
                     sq(w2.momentum) / w2.height
-                            + 0.5f * g * sq(w2.height)
                             - sq(w1.momentum) / w1.height
+                            + 0.5f * g * sq(w2.height)
                             - 0.5f * g * sq(w1.height)
             ) / dz;
         }
-        float damp = 0.1f;
+
+        // Full step
         for (int i = 0; i < numColumns - 2; i++) {
             WaterColumn w1 = waterColumns.get(i);
             WaterColumn w2 = waterColumns.get(i + 1);
 
             w2.height += -dt * (w2.midpointMomentum - w1.midpointMomentum) / dz;
             w2.momentum += -dt * (
-                    damp * w2.momentum + sq(w2.midpointMomentum) / w2.midpointHeight
-                            + .5 * g * sq(w2.midpointHeight)
+                    +sq(w2.midpointMomentum) / w2.midpointHeight
                             - sq(w1.midpointMomentum) / w1.midpointHeight
-                            - .5 * g * sq(w1.midpointHeight)
+                            + 0.5 * g * sq(w2.midpointHeight)
+                            - 0.5 * g * sq(w1.midpointHeight)
             ) / dz;
+
+            // Damping
+            w2.momentum += -dt * damp * w2.momentum / dz;
         }
 
-        // Boundary columns.
+        // Boundary columns
         WaterColumn w0 = waterColumns.get(0);
         WaterColumn w1 = waterColumns.get(1);
         WaterColumn w_1 = waterColumns.get(numColumns - 1);
         WaterColumn w_2 = waterColumns.get(numColumns - 2);
-        // Boundary conditions.
+
+        // Boundary conditions (reflective)
         w0.height = w1.height;
         w_1.height = w_2.height;
         w0.momentum = -w1.momentum;
         w_1.momentum = -w_2.momentum;
     }
 
-    void update() {
-        for (int i = 0; i < 40; i++) {
-            updateLoop();
-        }
-    }
-
     public void draw() {
         long start = millis();
         // update
         try {
-            if (updateClick) {
-                update();
+            if (isPlaying) {
+                for (int i = 0; i < 40; i++) {
+                    updateLoop();
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -168,21 +172,19 @@ public class WaterSimulation extends PApplet {
         background(0);
         drawWater();
         drawWaterTank();
-
         long draw = millis();
 
         surface.setTitle("Processing - FPS: " + Math.round(frameRate) + " Update: " + (update - start) + "ms Draw " + (draw - update) + "ms");
     }
 
     public void keyPressed() {
-        // Key Pressed
         if (key == 'u') {
-            updateClick = !updateClick;
+            isPlaying = !isPlaying;
         }
     }
 
     static public void main(String[] passedArgs) {
-        String[] appletArgs = new String[]{"demos.WaterSimulation"};
+        String[] appletArgs = new String[]{"demos.ShallowWater1D"};
         if (passedArgs != null) {
             PApplet.main(concat(appletArgs, passedArgs));
         } else {
